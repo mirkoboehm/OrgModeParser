a36dd2ac76e10d5e60c75b0b5f828ba713ebd563
Mirko Boehm (Endocode) <mirko@endocode.com>
2015-03-28 17:40:28 +0100

    Add exception path for handling errors.
    Simply basing it on the same exceptions as used by the parser, should
    be enough.

--- Demos/ClockTimeDemo/ClockTimeDemo-main.cpp
+++ Demos/ClockTimeDemo/ClockTimeDemo-main.cpp
@@ -4,8 +4,10 @@
 #include <QCommandLineParser>
 #include <QtDebug>
 
+#include <Exception.h>
 #include "ClockTimeSummary.h"
 
+using namespace OrgMode;
 using namespace std;
 
 int main(int argc, char *argv[])
@@ -13,29 +15,34 @@ int main(int argc, char *argv[])
     QCoreApplication a(argc, argv);
     QCoreApplication::setApplicationName(a.translate("main", "OrgModeParser clock time demo"));
     QCoreApplication::setApplicationVersion(a.translate("main", "1.0"));
-
     QCommandLineParser parser;
-    parser.setApplicationDescription(a.translate("main", "Clock time demo for the OrgModeParser."));
-    parser.addHelpOption();
-    parser.addVersionOption();
-    QCommandLineOption columnsOption(QStringList() << QStringLiteral("c") << QStringLiteral("columns"),
-                                     a.translate("main", "Terminal columns"),
-                                     a.translate("main", "columns"));
-    QCommandLineOption promptModeOption(QStringList() << QStringLiteral("p") << QStringLiteral("promptmode"),
-                                        a.translate("main", "Prompt mode (no newline at end)"));
-    parser.addOption(columnsOption);
-    parser.addOption(promptModeOption);
-    parser.process(a);
-    int columns;
-    if (parser.isSet(columnsOption)) {
-        bool ok;
-        columns = parser.value(columnsOption).toInt(&ok);
-        //if (!ok) bad();
-    } else {
-        columns = 60;
+    try {
+        parser.setApplicationDescription(a.translate("main", "Clock time demo for the OrgModeParser."));
+        parser.addHelpOption();
+        parser.addVersionOption();
+        QCommandLineOption columnsOption(QStringList() << QStringLiteral("c") << QStringLiteral("columns"),
+                                         a.translate("main", "Terminal columns."),
+                                         a.translate("main", "columns"));
+        QCommandLineOption promptModeOption(QStringList() << QStringLiteral("p") << QStringLiteral("promptmode"),
+                                            a.translate("main", "Prompt mode (no newline at end)."));
+        parser.addOption(columnsOption);
+        parser.addOption(promptModeOption);
+        parser.process(a);
+        int columns;
+        if (parser.isSet(columnsOption)) {
+            bool ok;
+            columns = parser.value(columnsOption).toInt(&ok);
+            if (!ok) {
+                throw RuntimeException(a.translate("main", "Columns argument needs to be an integer number!"));
+            }
+        } else {
+            columns = 60;
+        }
+        const bool promptMode = parser.isSet(promptModeOption);
+        ClockTimeSummary clocktime(parser.positionalArguments());
+        clocktime.report(promptMode, columns);
+    } catch (const RuntimeException& ex) {
+        wcerr << "Error: " << ex.message().toStdWString() << endl
+              << parser.helpText().toStdWString() << endl;
     }
-    const bool promptMode = parser.isSet(promptModeOption);
-
-    ClockTimeSummary clocktime(parser.positionalArguments());
-    clocktime.report(promptMode, columns);
 }
