c92d2d6ba49b7075c33d359ca19dcb78228e69fc
Mirko Boehm (Endocode) <mirko@endocode.com>
2015-03-28 11:28:33 +0100

    Simplify the clocklines test. Cleanup.

--- AutoTests/tst_ParserTests.cpp
+++ AutoTests/tst_ParserTests.cpp
@@ -22,6 +22,7 @@
 #include <FindElements.h>
 
 using namespace OrgMode;
+using namespace std;
 
 typedef void (*VerificationMethod)(const QByteArray& input, const QByteArray& output, OrgElement::Pointer element);
 Q_DECLARE_METATYPE(VerificationMethod)
@@ -124,44 +125,42 @@ void ParserTests::testParserAndIdentity_data()
     //Verification of the properties of SimpleTree.org:
     VerificationMethod testSimpleTree = [](const QByteArray&, const QByteArray&, OrgElement::Pointer element) {
         QCOMPARE(element->children().count(), 4);
-        QVERIFY(findElement<OrgMode::Headline>(element, QLatin1String("Headline 1")));
-        QVERIFY(findElement<OrgMode::Headline>(element, QLatin1String("Headline 1.1")));
-        QVERIFY(findElement<OrgMode::Headline>(element, QLatin1String("Headline 1.2")));
-        QVERIFY(findElement<OrgMode::Headline>(element, QLatin1String("Headline 2")));
-        QVERIFY(!findElement<OrgMode::Headline>(element, QLatin1String("Headline 3")));
+        QVERIFY(findElement<Headline>(element, QLatin1String("Headline 1")));
+        QVERIFY(findElement<Headline>(element, QLatin1String("Headline 1.1")));
+        QVERIFY(findElement<Headline>(element, QLatin1String("Headline 1.2")));
+        QVERIFY(findElement<Headline>(element, QLatin1String("Headline 2")));
+        QVERIFY(!findElement<Headline>(element, QLatin1String("Headline 3")));
         //This finds the opening line by regular expression:
-        QVERIFY(findElement<OrgMode::OrgLine>(element, QLatin1String("OrgMode file that the parser should parse")));
+        QVERIFY(findElement<OrgLine>(element, QLatin1String("OrgMode file that the parser should parse")));
         //This line does not exist:
-        QVERIFY(!findElement<OrgMode::OrgLine>(element, QLatin1String("There is no line like this")));
+        QVERIFY(!findElement<OrgLine>(element, QLatin1String("There is no line like this")));
     };
     QTest::newRow("SimpleTree") << FL1("://TestData/Parser/SimpleTree.org") << testSimpleTree;
 
     //Verify that CLOCK: lines are detected, parsed, and the numbers calculated and aggregated up the tree:
     VerificationMethod testClockEntries = [](const QByteArray&, const QByteArray&, OrgElement::Pointer element) {
         //Headline 1.1 contains one clock entry:
-        QCOMPARE(Clock(findElement<OrgMode::Headline>(element, QLatin1String("headline_1_1"))).duration(), 10 * 60);
+        QCOMPARE(Clock(findElement<Headline>(element, QLatin1String("headline_1_1"))).duration(), 10 * 60);
         //Headline 1.2 contains two clock entries that need to be accumulated:
-        QCOMPARE(Clock(findElement<OrgMode::Headline>(element, QLatin1String("headline_1_2"))).duration(), 20 * 60);
+        QCOMPARE(Clock(findElement<Headline>(element, QLatin1String("headline_1_2"))).duration(), 20 * 60);
         //Headline 1 is the parent of 1.1 and 1.2 and should have their times added up:
-        QCOMPARE(Clock(findElement<OrgMode::Headline>(element, QLatin1String("headline_1"))).duration(), 30 * 60);
+        QCOMPARE(Clock(findElement<Headline>(element, QLatin1String("headline_1"))).duration(), 30 * 60);
         //Verify parsing of the incomplete clock line that starts at 14:30 (child of headline_1_2)
-        auto const headline_1_2 = findElement<OrgMode::Headline>(element, QLatin1String("headline_1_2"));
+        auto const headline_1_2 = findElement<Headline>(element, QLatin1String("headline_1_2"));
         QVERIFY(headline_1_2);
-        auto complete = findElements<OrgMode::ClockLine>(headline_1_2);
+        auto complete = findElements<ClockLine>(headline_1_2);
         QCOMPARE(complete.size(), 2); // complete clock lines are also incomplete clock lines
-        auto allClockLines = findElements<OrgMode::IncompleteClockLine>(headline_1_2);
-        QCOMPARE(allClockLines.size(), 3); // complete clock lines are also incomplete clock lines
-        //Compute difference between the two sets of elements:
-        QVector<QSharedPointer<OrgMode::OrgElement>> difference(qMax(allClockLines.size(), complete.size()));
-        std::sort(allClockLines.begin(), allClockLines.end());
-        std::sort(complete.begin(), complete.end());
-        auto it = std::set_difference(allClockLines.begin(), allClockLines.end(),
-                                      complete.begin(), complete.end(), difference.begin());
-        difference.resize(it - difference.begin());
+        auto all = findElements<IncompleteClockLine>(headline_1_2);
+        QCOMPARE(all.size(), 3); // complete clock lines are also incomplete clock lines
+        //Store the difference between the two sets of elements in a vector:
+        sort(all.begin(), all.end());
+        sort(complete.begin(), complete.end());
+        vector<OrgElement::Pointer> difference;
+        set_difference(all.begin(), all.end(), complete.begin(), complete.end(), back_inserter(difference));
         //Verify:
         QVERIFY(difference.size()==1);
-        QVERIFY(!difference.at(0).dynamicCast<OrgMode::ClockLine>());
-        auto const incomplete = difference.at(0).dynamicCast<OrgMode::IncompleteClockLine>();
+        QVERIFY(!difference.at(0).dynamicCast<ClockLine>());
+        auto const incomplete = difference.at(0).dynamicCast<IncompleteClockLine>();
         QVERIFY(incomplete);
         QCOMPARE(incomplete->startTime(), QDateTime(QDate(2014, 9, 20), QTime(14, 30)));
     };
@@ -171,14 +170,14 @@ void ParserTests::testParserAndIdentity_data()
     VerificationMethod testTagParsing = [](const QByteArray&, const QByteArray&, OrgElement::Pointer element) {
         //Headline 1 only has TAG1:
         static QRegularExpression headline1Regex(QLatin1String("headline_1"));
-        Headline::Pointer headline_1 = findElement<OrgMode::Headline>(element, headline1Regex);
+        Headline::Pointer headline_1 = findElement<Headline>(element, headline1Regex);
         QVERIFY(headline_1);
         Tags tags_1(headline_1);
         QVERIFY(tags_1.hasTag(QLatin1String("TAG1")));
         QVERIFY(!tags_1.hasTag(QLatin1String("NONSENSE")));
         //Headline 1.1 is tagged TAG2 and inherits TAG1 from headline 1:
         static QRegularExpression headline1_1Regex(QLatin1String("headline_1_1"));
-        Headline::Pointer headline_1_1 = findElement<OrgMode::Headline>(element, headline1_1Regex);
+        Headline::Pointer headline_1_1 = findElement<Headline>(element, headline1_1Regex);
         QVERIFY(headline_1_1);
         Tags tags_1_1(headline_1_1);
         QVERIFY(tags_1_1.hasTag(QLatin1String("TAG1"))); // inherited
@@ -186,7 +185,7 @@ void ParserTests::testParserAndIdentity_data()
         QVERIFY(!tags_1_1.hasTag(QLatin1String("NONSENSE")));
         //Headline 1.2 has two tags (this tests parsing of multiple tags):
         static QRegularExpression headline1_2Regex(QLatin1String("headline_1_2"));
-        Headline::Pointer headline_1_2 = findElement<OrgMode::Headline>(element, headline1_2Regex);
+        Headline::Pointer headline_1_2 = findElement<Headline>(element, headline1_2Regex);
         QVERIFY(headline_1_2);
         Tags tags_1_2(headline_1_2);
         QVERIFY(tags_1_2.hasTag(QLatin1String("TAG1"))); // inherited
@@ -199,15 +198,15 @@ void ParserTests::testParserAndIdentity_data()
     //Verify parsing of file attributes (#+ATTRIBUTE: value):
     VerificationMethod testAttributeParsing = [](const QByteArray&, const QByteArray&, OrgElement::Pointer element) {
         //Test for an existing, non-empty attribute:
-        auto const attributeLine = findElement<OrgMode::FileAttributeLine>(element, FL1("DRAWERS"));
+        auto const attributeLine = findElement<FileAttributeLine>(element, FL1("DRAWERS"));
         QVERIFY(attributeLine);
         QCOMPARE(attributeLine->value(), FL1("MyDrawers TestDrawer"));
         //An existing but empty file attribute:
-        auto const emptyAttribute = findElement<OrgMode::FileAttributeLine>(element, FL1("EMPTY_ATTRIBUTE"));
+        auto const emptyAttribute = findElement<FileAttributeLine>(element, FL1("EMPTY_ATTRIBUTE"));
         QVERIFY(emptyAttribute);
         QVERIFY(emptyAttribute->value().isEmpty());
         //A non-existant file attribute:
-        auto const nonExistentAttribute = findElement<OrgMode::FileAttributeLine>(element, FL1("I DO NOT EXIST"));
+        auto const nonExistentAttribute = findElement<FileAttributeLine>(element, FL1("I DO NOT EXIST"));
         QVERIFY(!nonExistentAttribute);
     };
     QTest::newRow("AttributeParsing") << FL1("://TestData/Parser/DrawersAndProperties.org") << testAttributeParsing;
@@ -226,7 +225,7 @@ void ParserTests::testParserAndIdentity_data()
     //Verify calculation of attributes at file scope by the Properties class:
     VerificationMethod testFileAttributes = [](const QByteArray&, const QByteArray&, OrgElement::Pointer element) {
         //Headline 1 inherits the attributes from the attributes of the file it is in:
-        auto const headline_1 = findElement<OrgMode::Headline>(element, FL1("headline_1"));
+        auto const headline_1 = findElement<Headline>(element, FL1("headline_1"));
         QVERIFY(headline_1);
         Attributes attributes(headline_1);
         //A file level attribute:
@@ -246,34 +245,34 @@ void ParserTests::testParserAndIdentity_data()
     //Test two-pass parsing that provides the file properties first that will influence element parsing later:
     VerificationMethod testTwoPassParsing = [](const QByteArray&, const QByteArray&, OrgElement::Pointer element) {
         //Drawers are only identified if the first pass yielded a value for the #+DRAWERS: property
-        auto const headline_1 = findElement<OrgMode::Headline>(element, FL1("headline_1"));
+        auto const headline_1 = findElement<Headline>(element, FL1("headline_1"));
         QVERIFY(headline_1);
-        auto const myDrawer = findElement<OrgMode::Drawer>(headline_1, FL1("MyDrawers"));
+        auto const myDrawer = findElement<Drawer>(headline_1, FL1("MyDrawers"));
         QVERIFY(myDrawer);
         //Verify that content in drawer syntax is not considered a drawer if the name is not in #+DRAWERS:
-        auto const headline_2 = findElement<OrgMode::Headline>(element, FL1("headline_2"));
+        auto const headline_2 = findElement<Headline>(element, FL1("headline_2"));
         QVERIFY(headline_2);
-        auto const notADrawer = findElement<OrgMode::Drawer>(headline_2, FL1("NotADrawer"));
+        auto const notADrawer = findElement<Drawer>(headline_2, FL1("NotADrawer"));
         QVERIFY(!notADrawer);
     };
     QTest::newRow("TwoPassParsing") << FL1("://TestData/Parser/DrawersAndProperties.org") << testTwoPassParsing;
 
     //Test regular drawer parsing
     VerificationMethod testDrawerParsing = [](const QByteArray&, const QByteArray&, OrgElement::Pointer element) {
-        auto const headline_1 = findElement<OrgMode::Headline>(element, FL1("headline_1"));
+        auto const headline_1 = findElement<Headline>(element, FL1("headline_1"));
         QVERIFY(headline_1);
-        auto const myDrawer = findElement<OrgMode::Drawer>(headline_1, FL1("MyDrawers"));
+        auto const myDrawer = findElement<Drawer>(headline_1, FL1("MyDrawers"));
         QVERIFY(myDrawer);
         //Verify value of the "Monday" entry:
-        auto const mondayEntry = findElement<OrgMode::DrawerEntry>(headline_1, FL1("Monday"));
+        auto const mondayEntry = findElement<DrawerEntry>(headline_1, FL1("Monday"));
         QVERIFY(mondayEntry);
         QCOMPARE(mondayEntry->value(), FL1("yellow"));
         //Verify value of the "Sunday" entry, it is empty:
-        auto const sundayEntry = findElement<OrgMode::DrawerEntry>(headline_1, FL1("Sunday"));
+        auto const sundayEntry = findElement<DrawerEntry>(headline_1, FL1("Sunday"));
         QVERIFY(sundayEntry);
         QCOMPARE(sundayEntry->value(), FL1(""));
         //Check for a non-existant entry:
-        auto const thursdayEntry = findElement<OrgMode::DrawerEntry>(headline_1, FL1("Thursday"));
+        auto const thursdayEntry = findElement<DrawerEntry>(headline_1, FL1("Thursday"));
         QVERIFY(thursdayEntry == 0);
     };
     QTest::newRow("DrawerParsing") << FL1("://TestData/Parser/DrawersAndProperties.org") << testDrawerParsing;
@@ -281,22 +280,22 @@ void ParserTests::testParserAndIdentity_data()
     //Test corner cases of drawer parsing
     VerificationMethod testDrawerCornerCases = [](const QByteArray&, const QByteArray&, OrgElement::Pointer element) {
         //headline_1 has no drawer, because the drawer title line has text after the closing colon
-        auto const headline_1 = findElement<OrgMode::Headline>(element, FL1("headline_1"));
+        auto const headline_1 = findElement<Headline>(element, FL1("headline_1"));
         QVERIFY(headline_1);
-        auto const headline1TestDrawer = findElement<OrgMode::Drawer>(headline_1, FL1("TestDrawer"));
+        auto const headline1TestDrawer = findElement<Drawer>(headline_1, FL1("TestDrawer"));
         QVERIFY(!headline1TestDrawer);
         //headline_2 has a drawer. It has text after the closing :END:, but OrgMode accepts that
-        auto const headline_2 = findElement<OrgMode::Headline>(element, FL1("headline_2"));
+        auto const headline_2 = findElement<Headline>(element, FL1("headline_2"));
         QVERIFY(headline_2);
-        auto const headline2TestDrawer = findElement<OrgMode::Drawer>(headline_2, FL1("TestDrawer"));
+        auto const headline2TestDrawer = findElement<Drawer>(headline_2, FL1("TestDrawer"));
         QVERIFY(headline2TestDrawer);
-        auto const testEntry = findElement<OrgMode::DrawerEntry>(headline2TestDrawer, FL1("TestValue"));
+        auto const testEntry = findElement<DrawerEntry>(headline2TestDrawer, FL1("TestValue"));
         QVERIFY(testEntry);
         QCOMPARE(testEntry->value(), FL1("1"));
         //headline_3 does not contain a drawer, because a new headline starts in the middle of it:
-        auto const headline_3 = findElement<OrgMode::Headline>(element, FL1("headline_3"));
+        auto const headline_3 = findElement<Headline>(element, FL1("headline_3"));
         QVERIFY(headline_3);
-        auto const headline3TestDrawer = findElement<OrgMode::Drawer>(headline_3, FL1("TestDrawer"));
+        auto const headline3TestDrawer = findElement<Drawer>(headline_3, FL1("TestDrawer"));
         QVERIFY(!headline3TestDrawer);
 
     };
@@ -341,7 +340,7 @@ void ParserTests::testParserAndIdentity_data()
     //Verify calculation of properties for individual elements:
     VerificationMethod testDrawerEntries = [](const QByteArray&, const QByteArray&, OrgElement::Pointer element) {
         //Headline 1 contains a drawer "MyDrawers":
-        auto const headline_1 = findElement<OrgMode::Headline>(element, FL1("headline_1"));
+        auto const headline_1 = findElement<Headline>(element, FL1("headline_1"));
         QVERIFY(headline_1);
         const Properties properties1(headline_1);
         const Properties::Vector myDrawers = properties1.drawer(FL1("MyDrawers"));
@@ -357,7 +356,7 @@ void ParserTests::testParserAndIdentity_data()
     //Verify drawers are only detected as children of an element, not in the elements children:
     VerificationMethod testDrawerInHierarchy = [](const QByteArray&, const QByteArray&, OrgElement::Pointer element) {
         //Headline 2 does not have a drawer "MyDrawers", but it's child headline_2_1 does:
-        auto const headline_2 = findElement<OrgMode::Headline>(element, FL1("headline_2"));
+        auto const headline_2 = findElement<Headline>(element, FL1("headline_2"));
         const Properties properties_2(headline_2);
         try {
             properties_2.drawer(FL1("MyDrawers"));
@@ -365,7 +364,7 @@ void ParserTests::testParserAndIdentity_data()
         } catch(const RuntimeException& ex) {
             //qDebug() << qPrintable(ex.message());
         }
-        auto const headline_2_1 = findElement<OrgMode::Headline>(element, FL1("headline_2_1"));
+        auto const headline_2_1 = findElement<Headline>(element, FL1("headline_2_1"));
         const Properties properties_2_1(headline_2_1);
         auto const drawer_2_1 = properties_2_1.drawer(FL1("MyDrawers"));
         QCOMPARE(Attributes::attribute(drawer_2_1, FL1("Monday")), FL1("yellow"));
