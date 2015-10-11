004f81d9522cc7ed84c3b07d052110b1e44859b1
Mirko Boehm (Endocode) <mirko@endocode.com>
2015-10-11 15:18:00 +0200

    Add license information to sources and headers.

--- AutoTests/tst_Benchmarks.cpp
+++ AutoTests/tst_Benchmarks.cpp
@@ -1,3 +1,20 @@
+/** OrgModeParser - a parser for Emacs Org Mode files, written in C++.
+    Copyright (C) 2015 Mirko Boehm
+
+    This file is part of OrgModeParser.
+    OrgModeParser is free software: you can redistribute it and/or
+    modify it under the terms of the GNU General Public License as
+    published by the Free Software Foundation, version 3 of the
+    License.
+
+    OrgModeParser is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    See the GNU General Public License for more details. You should
+    have received a copy of the GNU General Public License along with
+    OrgModeParser. If not, see <http://www.gnu.org/licenses/>.
+*/
 #include <QString>
 #include <QtTest>
 
