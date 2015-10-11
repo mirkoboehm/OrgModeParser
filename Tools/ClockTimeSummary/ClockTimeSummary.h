c1b79595ade4631a5da52d491eb1054ee2a78586
Mirko Boehm (Endocode) <mirko@endocode.com>
2015-10-11 15:15:19 +0200

    The clock time summary is becoming a proper tool, not just a demo.

---
+++ Tools/ClockTimeSummary/ClockTimeSummary.h
@@ -0,0 +1,41 @@
+/** OrgModeParser - a parser for Emacs Org Mode files, written in C++.
+    Copyright (C) 2015 Mirko Boehm
+    
+    This file is part of OrgModeParser.
+    OrgModeParser is free software: you can redistribute it and/or
+    modify it under the terms of the GNU General Public License as
+    published by the Free Software Foundation, version 3 of the
+    License.
+
+    OrgModeParser is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+
+    See the GNU General Public License for more details. You should
+    have received a copy of the GNU General Public License along with
+    OrgModeParser. If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef CLOCKTIMESUMMARY_H
+#define CLOCKTIMESUMMARY_H
+
+#include <QObject>
+#include <OrgElement.h>
+
+class ClockTimeSummary : public QObject
+{
+    Q_OBJECT
+public:
+    explicit ClockTimeSummary(const QStringList& orgfiles, QObject *parent = 0);
+    int secondsClockedToday() const;
+    int secondsClockedThisWeek() const;
+
+    void report(bool promptMode, int columns);
+
+private:
+    QString hoursAndMinutes(int seconds);
+
+    OrgMode::OrgElement::Pointer toplevel_;
+
+};
+
+#endif // CLOCKTIMESUMMARY_H
